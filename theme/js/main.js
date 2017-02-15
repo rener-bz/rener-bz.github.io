99f5f60edae3d831fe52854f928b9cba9bf95ce1
rener-bz <rener.bezerra@gmail.com>
2017-02-15 18:07:22 -0200

    Add files via upload

--- theme/js/main.js
+++ theme/js/main.js
@@ -9,9 +9,8 @@ $('.hamburger').on('click touchstart', function(e){
     var value_idioma;
     $('.idioma-link').on('click touchstart', function(e){
         value_idioma= $(this).html();
-        alert (value_idioma);
         $(".btn-lang_desc").html(value_idioma);
-        if (value_idioma == "USA"){
+        if (value_idioma == "usa"){
             alert("usa");
             $("html").attr("lang","en");
         }
