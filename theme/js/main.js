71248526375fba511d515330ac39c495bfda9ada
rener-bz <rener.bezerra@gmail.com>
2017-02-16 16:50:56 -0200

    Add files via upload

--- theme/js/main.js
+++ theme/js/main.js
@@ -9,10 +9,14 @@ $('.hamburger').on('click touchstart', function(e){
     var value_idioma;
     $('.idioma-link').on('click touchstart', function(e){
         value_idioma= $(this).html();
+        alert (value_idioma);
         $(".btn-lang_desc").html(value_idioma);
-        if (value_idioma == "usa"){
-            alert("usa");
-            $("html").attr("lang","en");
+        if (value_idioma == "en"){
+            alert("Ingles");
+            $(".bio_desc").text("Lorem ispum");
+        }else{
+            alert("portugues");
+            $(".bio_desc").text("Desenvolvedor front-end, atualmente trabalha na Agência Giga, é entusiasta de UX design, tem interesse em arte, cultura, psicologia e uma boa leitura!");
         }
     });
 });
