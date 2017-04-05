103d4af4cf11bdc75c605b196ebe2fc6205ac3b2
rener-bz <rener.bezerra@gmail.com>
2017-04-05 11:51:21 -0300

    Add files via upload

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,6 +1,6 @@
 //Menu mobile
-$(document).ready(function(){
-$('.hamburger').on('click touchstart', function(e){  
+$(document).ready(function() {
+$('.hamburger').on('click touchstart', function (e) {
     $('.hamburger').toggleClass('is-active');
     $('html').toggleClass('menu-active');
     e.preventDefault();
@@ -9,29 +9,28 @@ $('.hamburger').on('click touchstart', function(e){
     var value_idioma;
     var name_idioma;
 //    Mudar idioma
-    $('.idioma-link').on('click touchstart', function(e){
-        value_idioma= $(this).attr('id');
-        name_idioma= $(this).text();
+    $('.idioma-link').on('click touchstart', function (e) {
+        value_idioma = $(this).attr('id');
+        name_idioma = $(this).text();
         
         $(".btn-lang_desc").html(value_idioma);
-        if (value_idioma == "en"){
+        if (value_idioma == "en") {
             $(".bio_desc").text("Front-end developer, now I'm working in Agência Giga, UX enthusiastic");
             $(".pdf_btn").attr("href","theme/pdf/rener-bezerra_english.pdf");
             $(".menu-tel").attr("href","tel:+5511953809720");
-        }else{
+        } else {
             $(".bio_desc").text("Desenvolvedor front-end, atualmente trabalha na Agência Giga, é entusiasta de UX design, tem interesse em arte, cultura, psicologia e uma boa leitura!");
             $(".pdf_btn").attr("href","theme/pdf/rener-bezerra.pdf");
             $(".menu-tel").attr("href","tel:11953809720");
         }
     });
 //    Apagar/acender a luz
-    $('.btn-light').on('click touchstart', function(e) {
+    $('.btn-light').on('click touchstart', function (e) {
         $('.btn-light_icon').toggleClass('fa-moon-o');
         $('.btn-light_icon').toggleClass('fa-sun-o');
         $('.body').toggleClass('body_dark');
         $('.menu-contato').toggleClass('menu-contato_dark');
         $('.pdf_btn').toggleClass('btn_dark');
-        alert ("luz");
         e.preventDefault();
     });
 });
